7dca9c26374c52869959537f93ec860a844a96c7
kapillohani023 <kapillohani023@gmail.com>
2025-06-19 01:10:19 +0530

    Add UI button component with unit tests

---
+++ src/app/shared/components/ui/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() text: string = 'Button';
+  @Input() disabled: boolean = false;
+  @Input() type: 'button' | 'submit'= 'button';
+  
+  @Output() onClick = new EventEmitter<Event>();
+
+  handleClick(event: Event): void {
+    if (!this.disabled) {
+      this.onClick.emit(event);
+    }
+  }
+}
